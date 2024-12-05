83e39d0587253877ca85411e0f7f99b4e24499d3
Clifford Beshers <beshers@seereason.com>
2024-12-05 10:25:03 -0800

    hix bootstrap

--- flake.nix
+++ flake.nix
@@ -1,42 +1,26 @@
 {
-  # This is a template created by `hix init`
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-    in
-      flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        overlays = [ haskellNix.overlay
-          (final: _prev: {
-            hixProject =
-              final.haskell-nix.hix.project {
-                src = ./.;
-                # uncomment with your current system for `nix flake show` to work:
-                #evalSystem = "x86_64-linux";
-              };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hixProject.flake {};
-      in flake // {
-        legacyPackages = pkgs;
-      });
-
-  # --- Flake Local Nix Configuration ----------------------------
-  nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    allow-import-from-derivation = "true";
+  description = "A Haskell project";
+  inputs.hix.url = "github:tek/hix";
+  outputs = {hix, ...}: hix.lib.flake {
+    packages = {
+      hello-ghcjs = {
+        src = ./.;
+        description = "Demonstrate a build bug somewhere in the haskell.nix ghcjs build chain,";
+        cabal = {
+          author = "Clifford Beshers";
+          build-type = "Simple";
+          license = "BSD-3-Clause";
+          version = "0.0.1";
+          meta = {
+            maintainer = "Clifford Beshers <clifford.beshers@gmail.com>";
+            homepage = "https://github.com/cliffordbeshers/hix-ghcjs-splitmix/";
+            synopsis = "A simple hix/ghcjs that fails to build";
+          };
+        };
+        executables.hello = {
+          source-dirs = "src";
+        };
+      };
+    };
   };
 }
